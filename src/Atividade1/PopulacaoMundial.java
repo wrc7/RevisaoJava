5818144305689f5092fd4ddd185d6473a464afe9
wrc7 <wrodrigueschoaste@gmail.com>
2020-08-31 11:11:23 -0300

    first commit

---
+++ src/Atividade1/PopulacaoMundial.java
@@ -0,0 +1,19 @@
+package Atividade1;
+
+public class PopulacaoMundial {
+    public static void main(String[] args) {
+//        int ano = Integer.parseInt(args[0]);
+        int anos = 2;
+        long populacaoMundial = 7807229720L;
+        double percentual = 1.05;
+
+        double divisao = percentual/100;
+
+        double total = divisao * populacaoMundial;
+
+        double estimativa = total * anos;
+
+        System.out.println(estimativa);
+
+    }
+}
